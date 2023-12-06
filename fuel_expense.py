5ef2b409df7798c445d2259a5fb6896098667e38
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2023-12-06 22:29:03 +0530

    Create fuel_expense.py
    Fuel expense calculator program using python language by using type casting of strings to integers

---
+++ fuel_expense.py
@@ -0,0 +1,8 @@
+mil=input('mileage per litre in km ? ')
+ful=input('fuel cost ? rs. ')
+kms=input('kms towards destination ? ')
+a=int(mil)
+b=int(ful)
+c=int(kms)
+d=c/a * b
+print('total fuel expenses = rs.',d)
