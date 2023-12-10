ba296764b114678fdd96c70877c711ee38879c51
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2023-12-10 16:03:16 +0530

    Create distance.py
    We can calculate the distance between two points in the graph by entering the value of two point (x1,x2) and (y1,y2)

---
+++ distance.py
@@ -0,0 +1,8 @@
+a=int(input('enter the number x1 '))
+b=int(input('enter the number x2 '))
+c=int(input('enter the number y1 '))
+d=int(input('enter the number y2 '))
+import math
+z=(b-a)**2 + (d-c)**2
+f=math.sqrt(z)
+print('the distance between 2 points is ',f)
