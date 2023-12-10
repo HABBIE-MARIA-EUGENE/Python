558587e282687fbe057ff63f1fa56b73b26add76
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2023-12-10 15:16:35 +0530

    Create greatest_num.py

---
+++ greatest_num.py
@@ -0,0 +1,9 @@
+a=int(input('enter the number a '))
+b=int(input('enter the number b '))
+c=int(input('enter the number c '))
+if (a>=b and a>=c):
+    print(a)
+elif (b>=a and b>=c):
+    print(b)
+else:
+    print(c)
