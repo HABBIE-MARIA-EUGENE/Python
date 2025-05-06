0985245a266698cb933b5cf24eac7daba149a0e0
HABBIE-MARIA-EUGENE <98164451+HABBIE-MARIA-EUGENE@users.noreply.github.com>
2025-05-06 18:54:10 +0530

    Create classes_obj.py

---
+++ classes_obj.py
@@ -0,0 +1,23 @@
+class goa:
+    name=""
+    drink=""
+    def party(self):
+        print("let`s do party")
+    def beach(self):
+        print("let`s enjoy the beach")
+
+
+ramesh=goa()
+suresh=goa()
+
+ramesh.name="ramesh"
+suresh.name="suresh"
+
+print(ramesh.name)
+print(suresh.name)
+
+ramesh.drink="yes"
+suresh.drink="no"
+
+print(ramesh.drink)
+print(suresh.drink)
